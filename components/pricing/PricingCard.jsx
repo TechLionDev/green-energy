9d2d52952d76e1316fe4909e82e8c461080a3492
TechLion Dev (via GIT) <git@techlion.dev>
2024-03-27 20:27:37 -0400

    Add className to PricingCard icon

--- components/pricing/PricingCard.jsx
+++ components/pricing/PricingCard.jsx
@@ -37,6 +37,7 @@ const PricingCard = ({ plan }) => {
                   stroke-width='2'
                   stroke-linecap='round'
                   stroke-linejoin='round'
+                  className="min-w-[24px]"
                 >
                   <path d='M22 11.08V12a10 10 0 1 1-5.93-9.14'></path>
                   <polyline points='22 4 12 14.01 9 11.01'></polyline>
